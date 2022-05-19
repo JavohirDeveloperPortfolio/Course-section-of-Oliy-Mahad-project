7e24ac34c28fa18c3f91065259e4e53f52529ce8
BUNYODBEK-RASULOV <7777777tegir@gmail.com>
2022-05-19 09:24:43 +0500

    QueueEntity success

---
+++ src/main/java/uz/oliymahad/courseservice/entity/quequeue/QueueEntity.java
@@ -0,0 +1,30 @@
+package uz.oliymahad.courseservice.entity.quequeue;
+import lombok.*;
+import uz.oliymahad.courseservice.entity.BaseEntity;
+import uz.oliymahad.courseservice.entity.course.CourseEntity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class QueueEntity extends BaseEntity {
+
+    @Column(nullable = false)
+    private long userId;
+
+    @Column(nullable = false)
+    private Date createdDate;
+
+    @Column(nullable = false)
+    private Date updatedDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderEnum orderEnum;
+
+    @ManyToOne
+    private CourseEntity courseEntity;
+}
