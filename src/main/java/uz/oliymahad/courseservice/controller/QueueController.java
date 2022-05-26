08b837918ab095049794d0f86a3a462d269e4641
Davron24 <davronsaydullayev1009@gmail.com>
2022-05-26 10:45:45 +0500

    queue controller changed

--- src/main/java/uz/oliymahad/courseservice/controller/QueueController.java
+++ src/main/java/uz/oliymahad/courseservice/controller/QueueController.java
@@ -1,73 +1,73 @@
 package uz.oliymahad.courseservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uz.oliymahad.courseservice.dto.ApiResponse;
 import uz.oliymahad.courseservice.dto.FilterQueueForGroupsDTO;
-import uz.oliymahad.courseservice.dto.GetUserCourseQueueDTO;
 import uz.oliymahad.courseservice.dto.QueueDto;
 import uz.oliymahad.courseservice.entity.quequeue.QueueEntity;
 import uz.oliymahad.courseservice.service.QueueService;
 
-import java.time.LocalDateTime;
 import java.util.List;
-
-import static uz.oliymahad.courseservice.controller.BaseController.API;
 
 
 @RestController
-@RequestMapping(API +"/queue")
+@RequestMapping("/queues")
 @RequiredArgsConstructor
 public class QueueController implements BaseController {
 
     private final QueueService queueService;
 
-    @PostMapping(ADD)
+    @PostMapping()
     public ResponseEntity<?> addQueue(@RequestBody QueueDto queueDto) {
         ApiResponse<Void> apiResponse = queueService.add(queueDto);
         return ResponseEntity.status(apiResponse.isStatus() ? HttpStatus.CREATED : HttpStatus.CONFLICT).body(apiResponse);
     }
 
-    @GetMapping(LIST)
+    @GetMapping()
     public ResponseEntity<?> getList (
           Pageable page
     ) {
         return ResponseEntity.ok(queueService.getList(page));
     }
 
-    @GetMapping(GET + "/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<?> getQueue (@PathVariable Long id) {
         ApiResponse<QueueDto> apiResponse = queueService.get(id);
         return ResponseEntity.status(apiResponse.isStatus() ? HttpStatus.OK : HttpStatus.NOT_FOUND).body(apiResponse);
     }
 
-    @PutMapping(UPDATE + "/{id}")
+    @PutMapping( "/{id}")
     public ResponseEntity<?> updateQueue (@PathVariable Long id, @RequestBody QueueDto queueDto) {
         ApiResponse<Void> apiResponse = queueService.edit(id, queueDto);
         return ResponseEntity.status(apiResponse.isStatus() ? HttpStatus.OK : HttpStatus.NOT_FOUND).body(apiResponse);
     }
 
-    @DeleteMapping(DELETE + "/{id}")
+
+
+    @DeleteMapping( "/{id}")
     public ResponseEntity<?> deleteQueue (@PathVariable Long id) {
         ApiResponse<Void> apiResponse = queueService.delete(id);
         return ResponseEntity.status(apiResponse.isStatus() ? HttpStatus.NO_CONTENT : HttpStatus.NOT_FOUND).body(apiResponse);
     }
 
-    @GetMapping(GET_USER_COURSE_QUEUE)
-    public ResponseEntity<?> getUserCourseQueue( @RequestBody GetUserCourseQueueDTO getUserCourseQueueDTO){
-        ApiResponse<List<Long>> apiResponse = queueService.getUserCourseQueue(getUserCourseQueueDTO.getUserId(), getUserCourseQueueDTO.getCourseId());
+    @GetMapping("/user/{userId}/course/{courseId}") //userning shu kursda neccinci orinda turganini korsatadi
+    public ResponseEntity<?> getUserCourseQueue(
+            @PathVariable Long userId,
+            @PathVariable Long courseId
+    ){
+        ApiResponse<List<Long>> apiResponse = queueService.getUserCourseQueue(userId,courseId);
         return ResponseEntity.status(apiResponse.isStatus() ? HttpStatus.OK : HttpStatus.NOT_FOUND).body(apiResponse);
     }
-
-    @GetMapping(GET_USERS_BY_FILTER)
-    public ResponseEntity<?> getUsersByFilter(@RequestBody FilterQueueForGroupsDTO filterQueueDTO){
-        ApiResponse<List<Long>> apiResponse = queueService.getUsersByFilter(filterQueueDTO);
-        return ResponseEntity.status(apiResponse.isStatus() ? HttpStatus.OK : HttpStatus.NOT_FOUND).body(apiResponse);
-    }
+//
+//    @GetMapping(GET_USERS_BY_FILTER)
+//    public ResponseEntity<?> getUsersByFilter(@RequestBody FilterQueueForGroupsDTO filterQueueDTO){
+//        ApiResponse<List<Long>> apiResponse = queueService.getUsersByFilter(filterQueueDTO);
+//        return ResponseEntity.status(apiResponse.isStatus() ? HttpStatus.OK : HttpStatus.NOT_FOUND).body(apiResponse);
+//    }
 
     @GetMapping(GET_QUEUES_BY_FILTER)
     public ResponseEntity<?> getQueuesByFilter(
